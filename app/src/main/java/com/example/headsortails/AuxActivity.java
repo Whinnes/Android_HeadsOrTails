4acb9a1eeb74960b0f3a232e7584e5007c7dc728
Whinnes <whinnes@gmail.com>
2019-03-09 19:10:58 -0500

    Implemented functions to transfer data between activities and finished app.

--- app/src/main/java/com/example/headsortails/AuxActivity.java
+++ app/src/main/java/com/example/headsortails/AuxActivity.java
@@ -2,12 +2,38 @@ package com.example.headsortails;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class AuxActivity extends AppCompatActivity {
+
+    private Button buttonBack;
+    private ImageView imageResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aux);
+
+        buttonBack = findViewById(R.id.buttonBack);
+        imageResult = findViewById(R.id.imageResult);
+
+
+        Bundle data = getIntent().getExtras();
+        int number = data.getInt("number");
+
+        if (number == 0){
+            imageResult.setImageResource(R.drawable.head_coin);
+        }else{
+            imageResult.setImageResource(R.drawable.tail_coin);
+        }
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
